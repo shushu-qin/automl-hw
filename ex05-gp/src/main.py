6a810a641c297ecd1d222f912274ff2654a42e99
s qin <s.qin-1@tudelft.com>
2021-10-21 14:46:53 +0200

    finish ex05 gaussian process

--- ex05-gp/src/main.py
+++ ex05-gp/src/main.py
@@ -3,6 +3,9 @@ import operator
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rcParams
+import time
+from scipy.optimize import minimize
+
 
 rcParams.update({'figure.autolayout': True})
 plt.style.use('seaborn-whitegrid')
@@ -31,8 +34,17 @@ def kernel_se(x_1, x_2, l, sigma_f):
     :param sigma_f:
     :return:
     """
-    raise NotImplementedError('Implement the squared exponential kernel')
-
+    l2 = np.ones(x_1.shape[1])/l/l
+    M = np.diag(l2)
+    K = np.zeros([x_1.shape[0], x_2.shape[0]])
+    for i in range(len(x_1)):
+        for j in range(len(x_2)):
+            x1i = x_1[i]
+            x2j = x_2[j]
+            dist = x1i-x2j
+            
+            K[i,j] = sigma_f**2 * (np.exp(-0.5*dist.T@ M @dist)).item()
+    return K
 
 def gp_prediction_a(data: np.ndarray, X: np.ndarray, phi: callable, sigma_n: float, Sigma_p: np.ndarray) \
         -> (np.ndarray, np.ndarray):
@@ -45,16 +57,26 @@ def gp_prediction_a(data: np.ndarray, X: np.ndarray, phi: callable, sigma_n: flo
     :param Sigma_p: covariance
     :return: mean and variance for all points in X
     """
-    # TODO compute parts
-    raise NotImplementedError()
+    # compute parts
+    X0, y0 = (zip(*data))
+    X0 = np.array(X0)
+    y0 = np.array(y0)
+    phi0 = phi(X0)
+    sigma_n2_inv = pow(sigma_n,-2)
+    A = sigma_n2_inv * phi0@phi0.T + np.linalg.inv(Sigma_p)
+    A_inv = np.linalg.inv(A)
 
     # for storing the results
     means = []
     variances = []
     # loop over all X
     for xs in X:
-        # TODO compute mean and variance
-        raise NotImplementedError()
+        phis = phi(xs)
+        mean = sigma_n2_inv*phis.T@A_inv@phi0@y0
+        var = phis.T @ A_inv @ phis
+        means.append(mean.item())
+        variances.append(var.item())
+
 
     return np.array(means), np.array(variances)
 
@@ -70,15 +92,27 @@ def gp_prediction_b(data: np.ndarray, X: np.ndarray, phi: callable, sigma_n: flo
     :param Sigma_p: covariance
     :return: mean and variance for all points in X
     """
-    # TODO compute parts
-    raise NotImplementedError()
+    # compute parts
+    X0, y0 = (zip(*data))
+    X0 = np.array(X0)
+    y0 = np.array(y0).reshape([-1,1])
+    phi0 = phi(X0)
+    sigma_n2 = pow(sigma_n,2)
+    n = y0.shape[0]
+    A = Sigma_p@phi0
+    B = phi0.T@Sigma_p@phi0 + sigma_n2*np.eye(n)
+    C = A@np.linalg.inv(B)
+
     # for storing the results
     means = []
     variances = []
     # loop over all X
     for xs in X:
-        # TODO compute mean and variance
-        raise NotImplementedError()
+        phis = phi(xs).reshape([-1,1])
+        mean = phis.T@C@y0
+        var = phis.T@Sigma_p@phis - phis.T@C@phi0.T@Sigma_p@phis
+        means.append(mean.item())
+        variances.append(var.item())
 
     return np.array(means), np.array(variances)
 
@@ -128,11 +162,15 @@ def negative_log_likelihood(data: np.ndarray):
         :return:
         """
         l, sigma_f, sigma_n = theta
-        raise NotImplementedError('Implement the negative log likelihood of the gaussian process (term 1).')
-        term_1 = None
+        K_f = kernel_se(X_train, X_train, l, sigma_f)
+        K_y = K_f + sigma_n ** 2 * np.eye(len(X_train))
+
+        L = np.linalg.cholesky(K_y)
+
+        alpha = np.linalg.inv(L.T)@np.linalg.inv(L)@Y_train
+        term_1 = (0.5*Y_train.T@alpha).item()
 
         # You are given term 2 and 3
-        L = np.linalg.cholesky(K_y)
         term_2 = np.sum(np.log(np.diag(L)))
 
         term_3 = 0.5 * len(X_train) * np.log(2 * np.pi)
@@ -169,17 +207,16 @@ def main():
     # compare the predictions
     mean1, var1 = gp_prediction_a(train_data, X, phi_n_of_x(num_features), 1.0, np.eye(num_features))
     mean2, var2 = gp_prediction_b(train_data, X, phi_n_of_x(num_features), 1.0, np.eye(num_features))
-    # TODO decide if mean and variance are equal
-    raise NotImplementedError()
-    print('Equal means: ', '?')
-    print('Equal variances: ', '?')
+    # decide if mean and variance are equal
+    print('Equal means: ', np.allclose(mean1, mean2))
+    print('Equal variances: ', np.allclose(var1, var2))
     print()
 
     # 'Warm-up' processor for fair time measurements
     for i in range(100):
         gp_prediction_a(train_data, X, phi_n_of_x(num_features), 1.0, np.eye(num_features))
 
-    # plot the computation time
+    plot the computation time
     tsa = []
     tsb = []
     dimensions = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
@@ -188,11 +225,14 @@ def main():
         I_n = np.eye(n)  # setup Identity of dimensionality n
         phi_n = phi_n_of_x(n)  # get basis functions
 
-        # TODO measure time to compute for implementation A
-        raise NotImplementedError()
-
-        # TODO measure time to compute for implementation B
-        raise NotImplementedError()
+        start = time.time()
+        gp_prediction_a(train_data, X, phi_n, 1.0, I_n)
+        run_time = time.time() - start
+        tsa.append(run_time)
+        start = time.time()
+        gp_prediction_b(train_data, X, phi_n, 1.0, I_n)
+        run_time = time.time() - start
+        tsb.append(run_time)
 
     plt.figure(figsize=(5, 5))
     plt.grid(True, which="both", ls="-", alpha=0.3)
@@ -233,7 +273,7 @@ def main():
     plt.title('GP Hyperparameter Optimization')
     plt.subplot(2, 1, 1)
     plt.title(
-        'INIT NLL: {:.3f}, l: {:.3f}, sigma_n: {:.3f}, sigma_f: {:.3f}'.format(nll_init[0], l_init, sigma_n_init,
+        'INIT NLL: {:.3f}, l: {:.3f}, sigma_n: {:.3f}, sigma_f: {:.3f}'.format(nll_init, l_init, sigma_n_init,
                                                                                sigma_f_init))
     plt.fill_between(X, m_init - 2 * np.sqrt(v_init), m_init + 2 * np.sqrt(v_init), alpha=0.2, color='g')
     plt.fill_between(X, m_init - 1 * np.sqrt(v_init), m_init + 1 * np.sqrt(v_init), alpha=0.2, color='g')
@@ -244,7 +284,7 @@ def main():
 
     plt.subplot(2, 1, 2)
     plt.title(
-        'OPT: NLL: {:.3f}, l: {:.3f}, sigma_n: {:.3f}, sigma_f: {:.3f}'.format(nll_opt[0], l_opt, sigma_n_opt,
+        'OPT: NLL: {:.3f}, l: {:.3f}, sigma_n: {:.3f}, sigma_f: {:.3f}'.format(nll_opt, l_opt, sigma_n_opt,
                                                                                sigma_f_opt))
     plt.fill_between(X, m_opt - 2 * np.sqrt(v_opt), m_opt + 2 * np.sqrt(v_opt), alpha=0.2, color='g')
     plt.fill_between(X, m_opt - 1 * np.sqrt(v_opt), m_opt + 1 * np.sqrt(v_opt), alpha=0.2, color='g')
